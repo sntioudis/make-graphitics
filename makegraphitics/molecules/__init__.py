b7c9873707ed318ee24c324e2ef2ae4e1cd4d955
Stavros Ntioudis <42137665+sntioudis@users.noreply.github.com>
2026-02-26 11:03:52 +0000

    Fixed compatibility with python3

--- makegraphitics/molecules/__init__.py
+++ makegraphitics/molecules/__init__.py
@@ -1,6 +1,6 @@
 from .graphite_cell import Graphite
 from .base import Molecule
 from .hexagon_graphene import Hexagon_Graphene
-from hexagon_graphene_sheet import Hexagon_Graphene_Sheet
+from .hexagon_graphene_sheet import Hexagon_Graphene_Sheet
 from .graphene_cell import Graphene
 from .rectangle_graphene import Rectangle_Graphene
